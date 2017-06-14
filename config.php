02175900fa8b46be6a40dca201f5b33f1a8b7e34
Peter 4A5A <498883@edu.rocmn.nl>
2017-06-14 10:52:37 +0200

    Added config

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+  $config['base_url'] = '/leerjaar2/php/read-url/';
+
+
+
+?>
--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
 
   require_once 'model/Router.class.php';
+  require_once 'config.php';
+
   $Router = new Router();
 
   $Router->defaultController = 'order';
   $Router->defaultMethod = 'default';
-  $Router->siteLocation = '/leerjaar2/php/read-url/';
+  $Router->siteLocation = $config['base_url'];
 
   $Router->procesTheURL();
   $Router->debug();
