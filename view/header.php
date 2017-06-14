bd264c9f2e2fabb4e87ebf1d64cf66e660d252a0
Peter 4A5A <498883@edu.rocmn.nl>
2017-06-14 11:31:07 +0200

    Changed to php
    And added the correct path to the style

---
+++ view/header.php
@@ -0,0 +1,13 @@
+    <!DOCTYPE html>
+    <html>
+      <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Router</title>
+        <link rel="stylesheet" href="<?php echo base_url ?>/view/style/grid.css" type="text/css">
+        <link rel="stylesheet" href="<?php echo base_url ?>/view/style/style.css" type="text/css">
+      </head>
+    <body>
+
+    <div class="header">
+      <h1>Chania</h1>
+    </div>
