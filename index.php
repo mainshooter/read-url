25217ace79f62b8d83afbd80d7b2f805d7501a7a
Peter 4A5A <498883@edu.rocmn.nl>
2017-06-14 09:52:06 +0200

    You can set the default controller and method
    Plus debug :D

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 
   require_once 'model/Router.class.php';
-  $Router = new Router('order/');
+  $Router = new Router();
+  $Router->defaultController = 'order';
+  $Router->defaultMethod = 'default';
 
   $Router->procesTheURL();
+  $Router->debug();
 
 ?>
