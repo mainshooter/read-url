841b2a0f033ad18ae8c75350d90480fd4647c03a
Peter 4A5A <498883@edu.rocmn.nl>
2017-06-14 13:52:46 +0200

    Styling

--- view/header.php
+++ view/header.php
@@ -11,14 +11,14 @@
     <div class="header">
       <h1><a href="<?php echo base_url ?>">Order list</a></h1>
     </div>
+    <div class="col-3 col-m-3 menu">
+      <ul>
+        <li>The Flight</li>
+        <li>The City</li>
+        <li>The Island</li>
+        <li>The Food</li>
+      </ul>
+    </div>
     <div class="row">
       <div class="col-3 col-m-12">
-    <div class="aside">
-      <h2>What?</h2>
-      <p>Chania is a city on the island of Crete.</p>
-      <h2>Where?</h2>
-      <p>Crete is a Greek island in the Mediterranean Sea.</p>
-      <h2>How?</h2>
-      <p>You can reach Chania airport from all over Europe.</p>
-    </div>
   </div>
