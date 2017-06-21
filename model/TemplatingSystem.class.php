3aea432ac742ac5fb0d8eace5951c1e1461a39cb
Peter 4A5A <498883@edu.rocmn.nl>
2017-06-21 09:11:40 +0200

    builded the basic construct

--- model/TemplatingSystem.class.php
+++ model/TemplatingSystem.class.php
@@ -5,13 +5,23 @@
     private $error = '';
 
 
-    function __construct() {
-      if (file_exists($this->template)) {
-        $this->error = 0;
+    /**
+     * checks if the file exists
+     * @param boolean $template [The template name]
+     */
+    function __construct($template = false) {
+      if (file_exists($template)) {
+        if (pathinfo($template)[2] == 'tpl') {
+          // To check the tamplate extension we check on the [2] position of the pathinfo()
+          $this->template = $template;
+        }
+        else {
+          $this->error = "Wrong extension it isn't tpl";
+        }
       }
 
       else {
-        $this->error = 1;
+        $this->error = "File doesn't exists";
       }
 
     }
