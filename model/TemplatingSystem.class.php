7ace528b9386f8a143a8c152b2cc6276b7562b9a
Peter 4A5A <498883@edu.rocmn.nl>
2017-06-21 09:41:44 +0200

    Added the checks to the constructor

--- model/TemplatingSystem.class.php
+++ model/TemplatingSystem.class.php
@@ -1,30 +1,48 @@
 <?php
+  // require_once 'filehandler.class.php';
 
   class TemplatingSystem {
     public $template = 'default.tpl';
+    private $templateLayout;
+
+    public $canWeRun;
     private $error = '';
 
 
     /**
      * checks if the file exists
-     * @param boolean $template [The template name]
+     * @param string $template [The template name]
      */
     function __construct($template = false) {
-      if (file_exists($template)) {
-        if (pathinfo($template)[2] == 'tpl') {
-          // To check the tamplate extension we check on the [2] position of the pathinfo()
-          $this->template = $template;
+      if ($template) {
+        if (file_exists($template)) {
+          if (pathinfo($template)[2] == 'tpl') {
+            // To check the template extension we check on the [2] position of the pathinfo()
+            $this->template = $template;
+            $this->canWeRun = true;
+          }
+          else {
+            $this->error = "Wrong extension it isn't tpl";
+            $this->canWeRun = false;
+          }
         }
+
         else {
-          $this->error = "Wrong extension it isn't tpl";
+          $this->error = "File doesn't exists";
+          $this->canWeRun = false;
         }
       }
-
-      else {
-        $this->error = "File doesn't exists";
-      }
-
     }
+
+    /**
+     * [readTemplate description]
+     * @return [type] [description]
+     */
+    public function readTemplate() {
+      $this->templateLayout = file_get_contents('view/template/' . $this->template);
+    }
+
+
   }
 
 ?>
